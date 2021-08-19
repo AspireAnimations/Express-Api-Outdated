dc6cc59d3eff550512aa512e858696abdc92f2c6
Amarpreet Singh <animate@aspireanimations.com>
2021-08-19 19:20:53 +0530

    Express Api Outdated

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger')
+
+const app = express();
+
+
+// const logger = (req, res, next) => {
+//   console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}:${moment().format()}`);
+//   next();
+// };
+
+// Init Middleware
+// app.use(logger);
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/members', require('./routes/api/members'))
+
+
+
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
